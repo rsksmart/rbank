5059a4c96a084f2b93c11626fd22185680303a68
Anni Piragauta <aapiragautau@unal.edu.co>
2020-04-15 18:01:18 -0500

    Adding Deposit/Lending view
    	I've created the deposi/lending view to the router
    	to be listed in the drawer, with all the content
    	specified on the RBank wireframes.

--- dapp/src/router/routes.js
+++ dapp/src/router/routes.js
@@ -14,9 +14,15 @@ export default [
     icon: 'dashboard',
   },
   {
+    path: '/depositLending',
+    name: 'Lending / borrow',
+    component: () => import(/* webpackChunkName: "dashboard" */ '@/views/DepositLending.vue'),
+    icon: 'local_atm',
+  },
+  {
     path: '/account',
     name: 'Account',
     component: () => import(/* webpackChunkName: "account" */ '@/views/Account.vue'),
-    icon: 'attach_money',
+    icon: 'person',
   },
 ];
